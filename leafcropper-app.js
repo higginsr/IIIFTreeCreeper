261af34ff66c6bbe24e757353fd08b7c9b6b5db0
higginsr <r.i.higgins@durham.ac.uk>
2019-03-17 09:44:59 +0000

    Clarify url parameters

--- leafcropper-app.js
+++ leafcropper-app.js
@@ -2,12 +2,12 @@ function getQueryStringValue (key) {
   return decodeURIComponent(window.location.search.replace(new RegExp("^(?:.*[&\\?]" + encodeURIComponent(key).replace(/[\.\+\*]/g, "\\$&") + "(?:\\=([^&]*))?)?.*$", "i"), "$1"));  
 }
 
-var manifest_url = getQueryStringValue("umfst");
+var manifest_url = getQueryStringValue("manifest");
 
-var canvas_url = getQueryStringValue("ucnvs");
+var canvas_url = getQueryStringValue("canvas");
 
 // var jp2kfilename = decodeURIComponent(window.location.search.substr(1)).slice(2);
-var jp2kfilename = getQueryStringValue("ujp2k");
+var jp2kfilename = getQueryStringValue("jp2k");
 var baseUrl = jp2kfilename;
 
 var map = L.map('map', {
